af24a5a764950abda8a3c4a72122a67a69d860f1
Superdan-xtu <shudan.tian@xtu.edu.cn>
2026-06-11 10:17:26 +0800

    Add files via upload

---
+++ com_quad.m
@@ -0,0 +1,23 @@
+function [ f ] = com_quad(alpha)
+%COM_QUAD Summary of this function goes here
+%   Detailed explanation goes here
+
+n = size(alpha,1)-1;
+for i = 1:size(alpha)
+    if alpha(i)<0
+        f=0;
+        n=10000;
+    end
+end
+
+
+switch n
+case 1
+f = factorial(alpha(1))*factorial(alpha(2))/factorial(sum(alpha)+n);
+case 2
+f = factorial(alpha(1))*factorial(alpha(2))*factorial(alpha(3))*factorial(n)/factorial(sum(alpha)+n);
+case 3 
+f = factorial(alpha(1))*factorial(alpha(2))*factorial(alpha(3))*factorial(alpha(4))*factorial(n)/factorial(sum(alpha)+n);
+end
+end
+
