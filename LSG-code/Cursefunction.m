532525539c17cc9aac7fd1ddf131043d65c6a4b5
Superdan-xtu <shudan.tian@xtu.edu.cn>
2026-06-22 20:23:49 +0800

    Rename Cursefunction.m to LSG-code/Cursefunction.m

--- LSG-code/Cursefunction.m
+++ LSG-code/Cursefunction.m
@@ -1,6 +1,6 @@
 function [ phi1,phi2,phi3 ] = Cursefunction( node, elem, area, pxy )
-%CURSEFUNCTION 此处显示有关此函数的摘要
-%   此处显示详细说明
+%CURSEFUNCTION 姝ゅ鏄剧ず鏈夊叧姝ゅ嚱鏁扮殑鎽樿
+%   姝ゅ鏄剧ず璇︾粏璇存槑
 x = pxy(:,1);
 y = pxy(:,2);
 v1 = node(elem(:,1),:);
