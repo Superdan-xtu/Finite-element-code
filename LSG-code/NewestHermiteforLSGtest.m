d1647092d863d362bb7efe99d1ffd21de695791e
Superdan-xtu <shudan.tian@xtu.edu.cn>
2026-06-22 20:31:51 +0800

    Rename NewestHermiteforLSGtest.m to LSG-code/NewestHermiteforLSGtest.m

--- LSG-code/NewestHermiteforLSGtest.m
+++ LSG-code/NewestHermiteforLSGtest.m
@@ -72,7 +72,7 @@ Ndof = double(max(elem2dof1(:)));
 elem2dof = [elem2dof1];
 Stiff = sparse(Ndof,Ndof);
 M =  Stiff;
-% 统一法向
+% ç»Ÿä¸€æ³•å‘
 elemnor = elemn;
 elemn(:,:,1)=elemn(:,:,1).*elem2edgeSign(:,1);
 elemn(:,:,2)=elemn(:,:,2).*elem2edgeSign(:,2);
@@ -178,7 +178,7 @@ A(10:12,:) = BAE;
 A(13:15,:)=BA;
 
 cof = inv(A);
-COF((i-1)*base_num+1:i*base_num,:)=cof'; %每一行代表一个单元的�?个基的系�?
+COF((i-1)*base_num+1:i*base_num,:)=cof'; %æ¯ä¸€è¡Œä»£è¡¨ä¸€ä¸ªå•å…ƒçš„ä¸?ä¸ªåŸºçš„ç³»æ•?
 
 end
 clear cof BA A BAE
@@ -284,7 +284,7 @@ end
 clear AA MM
 %  
    
-%% 组装刚度矩阵 
+%% ç»„è£…åˆšåº¦çŸ©é˜µ 
 %DPHI = zeros(10,10);
 Dx = zeros(NT,base_num);
 Dy = zeros(NT,base_num);
@@ -377,7 +377,7 @@ end
 % 
  end      
 %Stiff = Stiff + (1/hsize^5)*pA;
-%% 右端�?        
+%% å³ç«¯é¡?        
 %big_Stiff = [iota*Stiff+M, sparse(Ndof,Ndof),sparse(Ndof,Ndof);sparse(Ndof,Ndof),iota*Stiff+M,sparse(Ndof,Ndof);sparse(Ndof,Ndof),sparse(Ndof,Ndof),2*iota*Stiff+2*M];%blkdiag(Stiff,Stiff);
 
 beta_m = (2*mu+La)/(4*mu*(La+mu));
